4097ca8af923a4d6bbdc4285671f069931b3f296
Maycon Santos <maycon.s.santos44@gmail.com>
2021-07-07 20:05:43 -0300

    Página criada com sucesso

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.scss";
+import backImage from "./assets/back-image.svg";
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src={backImage} alt="" className={styles.backImage} />
+      <img src={logo} alt="" className={styles.logo} />
+      <h1>Autenticação feita.</h1>
+      <h3>Você pode fechar essa página</h3>
+    </>
   );
 }
 
